f53c21970087a0d6dae28bcb9f860bbce04e4f44
kosinovdaniil <kosinov.daniil.g@gmail.com>
2016-02-15 23:38:04 +0300

    Add basic architecture

--- Epam.Wunderlist.Kosinov.Klimchuk/BLL.Interfacies/Services/IContentSearchService.cs
+++
@@ -1,9 +0,0 @@
-﻿using System.Collections.Generic;
-
-namespace BLL.Interface.Services
-{
-    public interface IContentSearchService
-    {
-        IEnumerable<int> Search(string input);
-    }
-}
