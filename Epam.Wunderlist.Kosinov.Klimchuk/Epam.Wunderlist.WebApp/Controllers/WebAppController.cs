3a886dbc360ced325fd057a59865375bd76c7e42
Анна Климчук <klimchuk.anuta@gmail.com>
2016-02-28 19:01:06 +0300

    add layout

---
+++ Epam.Wunderlist.Kosinov.Klimchuk/Epam.Wunderlist.WebApp/Controllers/WebAppController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Epam.Wunderlist.WebApp.Controllers
+{
+    public class WebAppController : Controller
+    {
+        // GET: WebApp
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
